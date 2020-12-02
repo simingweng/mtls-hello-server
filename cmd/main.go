d0782a32775a934cb2430866d40ef111cf21994c
SiMing Weng <siming.weng@gmail.com>
2020-12-02 16:26:42 -0500

    serve HTTP also

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"sync"
 )
 
 func main() {
@@ -16,23 +17,44 @@ func main() {
 			http.Error(writer, "failed to write response greeting", http.StatusInternalServerError)
 		}
 	})
-	cp, err := x509.SystemCertPool()
-	if err != nil {
-		log.Fatalln("failed to get system certificate pool")
-	}
-	ca, err := ioutil.ReadFile("/etc/tls/ca.crt")
-	if err != nil {
-		log.Fatalln("failed to read CA certificate")
-	}
-	cp.AppendCertsFromPEM(ca)
-	err = (&http.Server{
-		Addr: ":8443",
-		TLSConfig: &tls.Config{
-			ClientAuth: tls.RequireAndVerifyClientCert,
-			ClientCAs:  cp,
-		},
-	}).ListenAndServeTLS("/etc/tls/tls.crt", "/etc/tls/tls.key")
-	if err != nil {
-		log.Fatalln("failed to start server")
-	}
+
+	var wg sync.WaitGroup
+
+	// start HTTP server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// start HTTPS server
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		cp, err := x509.SystemCertPool()
+		if err != nil {
+			log.Println("failed to get system certificate pool")
+		}
+		ca, err := ioutil.ReadFile("/etc/tls/ca.crt")
+		if err != nil {
+			log.Println("failed to read CA certificate")
+		}
+		cp.AppendCertsFromPEM(ca)
+		err = (&http.Server{
+			Addr: ":8443",
+			TLSConfig: &tls.Config{
+				ClientAuth: tls.RequireAndVerifyClientCert,
+				ClientCAs:  cp,
+			},
+		}).ListenAndServeTLS("/etc/tls/tls.crt", "/etc/tls/tls.key")
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	wg.Wait()
+	log.Fatalln("both HTTP and HTTPS server are down")
 }
